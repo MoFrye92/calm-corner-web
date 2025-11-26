bcf34daee29ac1be733b61975dd657d3fdba2ee9
Mo Frye <119350975+MoFrye92@users.noreply.github.com>
2025-11-26 12:28:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// --- Firebase Setup ---
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfVOR--E6CgaLNopCql4QNviXdhVg4GAY",
+  authDomain: "calm-corner-e92e0.firebaseapp.com",
+  projectId: "calm-corner-e92e0",
+  storageBucket: "calm-corner-e92e0.firebasestorage.app",
+  messagingSenderId: "470025194370",
+  appId: "1:470025194370:web:90830dd0951427b39a1d9b",
+  measurementId: "G-HBC6NHQF19"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+
 // Save mood on click
 function selectMood(mood) {
     localStorage.setItem("currentMood", mood);
